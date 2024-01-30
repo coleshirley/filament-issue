9750a9d568c183483614cd073b22d63ea156b984
Cole Shirley <cole.shirley@artificertech.com>
2024-01-29 18:56:58 -0600

    simplify example

--- app/Filament/Widgets/SelectGroupErrorWithDisableOption.php
+++ app/Filament/Widgets/SelectGroupErrorWithDisableOption.php
@@ -17,29 +17,31 @@ class SelectGroupErrorWithDisableOption extends Widget implements HasForms
 
     protected static string $view = 'filament.widgets.select-group-error-with-disable-option';
 
+
+    public array $data = [];
+
     public function form(Form $form): Form
     {
         return $form->schema([
-            Repeater::make('test')
-                ->schema([
-
-                    Select::make('select')
-                        ->required()
-                        ->options([
-                            'Group 1' => [
-                                1 => 'A',
-                            ],
-                            'Group 2' => [
-                                2 => 'B',
-                            ],
-                        ])
-                        ->disableOptionWhen(
-                            fn (string $value) => $value == 1
-                        )
-                        ->in(fn (Select $component): array => dump(array_keys($component->getEnabledOptions()))),
-                ])->afterStateUpdated(function () {
+            Select::make('select')
+                ->live()
+                ->required()
+                ->options([
+                    'Group 1' => [
+                        1 => 'A',
+                    ],
+                    'Group 2' => [
+                        2 => 'B',
+                    ],
+                ])
+                ->disableOptionWhen(
+                    fn (string $value) => $value == 1
+                )
+                ->in(fn (Select $component): array => array_keys($component->getEnabledOptions()))
+                ->afterStateUpdated(function () {
                     $this->form->validate();
                 }),
-        ]);
+        ])
+            ->statePath('data');
     }
 }
